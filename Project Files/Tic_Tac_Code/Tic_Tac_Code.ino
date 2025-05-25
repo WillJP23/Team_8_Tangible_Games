51595b108c34c8cb9dddd37b41f8146001a4829c
WillJP23 <WILL.PEARSALL23@bathspa.ac.uk>
2025-05-25 19:15:36 +0100

    added led code

--- Project Files/Tic_Tac_Code/Tic_Tac_Code.ino
+++ Project Files/Tic_Tac_Code/Tic_Tac_Code.ino
@@ -167,7 +167,7 @@ void loop()
     Serial.println(pos);
   }
   //determines which square is chosen
-  //can add code below this section into this part to make more effecient
+
   if ((pos == '1') && (r1 == false) && (g1 == false)) {
     if ((greenred == false) && (g1 == false)) {
     digitalWrite(13, HIGH);
@@ -182,30 +182,118 @@ void loop()
     message = false;
     pos1 = false;
   }
-    //here for example: if / else if
-  else if (pos == '2') {
-    pos2 == true;
+  else if ((pos == '2') && (r2 == false) && (g2 == false)) {
+    if ((greenred == false) && (g2 == false)) {
+    digitalWrite(9, HIGH);
+    greenred = true;
+    r2 = true;
+    }
+    if ((greenred == true) && (r2 == false)) {
+    digitalWrite(8, HIGH);
+    greenred = false;
+    g2 = true;
+    }
+    message = false;
+    pos2 = false;
   }
-  else if (pos == '3') {
-    pos3 == true;
+  else if ((pos == '3') && (r3 == false) && (g3 == false)) {
+    if ((greenred == false) && (g3 == false)) {
+    digitalWrite(12, HIGH);
+    greenred = true;
+    r3 = true;
+    }
+    if ((greenred == true) && (r3 == false)) {
+    digitalWrite(10, HIGH);
+    greenred = false;
+    g3 = true;
+    }
+    message = false;
+    pos3 = false;
   }
-  else if (pos == '4') {
-    pos4 == true;
+  else if ((pos == '4') && (r4 == false) && (g4 == false)) {
+    if ((greenred == false) && (g4 == false)) {
+    digitalWrite(7, HIGH);
+    greenred = true;
+    r4 = true;
+    }
+    if ((greenred == true) && (r4 == false)) {
+    digitalWrite(5, HIGH);
+    greenred = false;
+    g4 = true;
+    }
+    message = false;
+    pos4 = false;
   }
-  else if (pos == '5') {
-    pos5 == true;
+  else if ((pos == '5') && (r5 == false) && (g5 == false)) {
+    if ((greenred == false) && (g5 == false)) {
+    digitalWrite(6, HIGH);
+    greenred = true;
+    r5 = true;
+    }
+    if ((greenred == true) && (r5 == false)) {
+    digitalWrite(4, HIGH);
+    greenred = false;
+    g5 = true;
+    }
+    message = false;
+    pos5 = false;
   }
-  else if (pos == '6') {
-    pos6 == true;
+  else if ((pos == '6') && (r6 == false) && (g6 == false)) {
+    if ((greenred == false) && (g6 == false)) {
+    digitalWrite(3, HIGH);
+    greenred = true;
+    r6 = true;
+    }
+    if ((greenred == true) && (r6 == false)) {
+    digitalWrite(2, HIGH);
+    greenred = false;
+    g6 = true;
+    }
+    message = false;
+    pos6 = false;
   }
-  else if (pos == '7') {
-    pos7 == true;
+  else if ((pos == '7') && (r7 == false) && (g7 == false)) {
+    if ((greenred == false) && (g7 == false)) {
+    digitalWrite(A0, HIGH);
+    greenred = true;
+    r7 = true;
+    }
+    if ((greenred == true) && (r7 == false)) {
+    digitalWrite(A2, HIGH);
+    greenred = false;
+    g7 = true;
+    }
+    message = false;
+    pos7 = false;
   }
-  else if (pos == '8') {
-    pos8 = true;
+  else if ((pos == '8') && (r8 == false) && (g8 == false)) {
+    if ((greenred == false) && (g8 == false)) {
+    digitalWrite(A1, HIGH);
+    greenred = true;
+    r8 = true;
+    }
+    if ((greenred == true) && (r8 == false)) {
+    digitalWrite(A3, HIGH);
+    greenred = false;
+    g8 = true;
+    }
+    message = false;
+    pos8 = false;
   }
-  else if (pos == '9') {
-    pos9 == true;
+  else if ((pos == '9') && (r9 == false) && (g9 == false)) {
+    if ((greenred == false) && (g9 == false)) {
+    digitalWrite(A4, HIGH);
+    greenred = true;
+    r9 = true;
+    }
+    if ((greenred == true) && (r9 == false)) {
+    digitalWrite(A5, HIGH);
+    greenred = false;
+    g9 = true;
+    }
+    message = false;
+
+    pos9 = false;
   }
   else {
     //make sure this works
